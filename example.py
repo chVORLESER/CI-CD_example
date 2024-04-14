2c5521a548fd61c5fbf4cac44ef3c9a325451f93
佩佩 <55478368+chVORLESER@users.noreply.github.com>
2024-04-14 12:44:45 +0800

    Add files via upload

---
+++ example.py
@@ -0,0 +1,11 @@
+import sys, os
+
+message = "I am confident to be successful in the Cloud exam"
+
+message = message.replace("Cloud", "cwa")
+
+print(message)
+
+if __name__ == '__main__':
+    print('hello world')
+
